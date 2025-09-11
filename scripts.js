c6a8506e4c9b5bca6d9b5c12dc324c4ad3cc223a
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-09-11 22:16:00 +0200

    add fetch with POST method

--- scripts.js
+++ scripts.js
@@ -3,9 +3,28 @@
 let form = document.getElementsByClassName("register-form")[0];
 form.addEventListener("submit", submitForm);
 
-function submitForm(event) {
+async function submitForm(event) {
     event.preventDefault();
+
+    const url = "https://visitregistration-a9btgsd0ephsbade.swedencentral-01.azurewebsites.net/api/httpformtrigger"
+
+    var email = document.getElementById("email-input").value;
+    var name = document.getElementById("name-input").value;
+    var reason = document.getElementById("reason-input").value;
+
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            email: email,
+            name: name,
+            reason: reason
+        })
+    });
+
+    if (!response.ok) {
+        text.innerText = "Please try again."
+    }
+
     let text = document.getElementsByClassName("info-message")[0];
-    text.innerText = "You pressed the button"
-    console.log("Hello world!")
+    text.innerText = "Your visit has been registrated"
 }
